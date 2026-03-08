2d0235e2b98fd013b416da0f085a6642d3fe74b5
Niklas Rammerstorfer <rammerstorfer.n@gmail.com>
2026-03-08 11:16:29 +0100

    Beautified the UI.

--- app/src/main/java/com/ramniksoftware/chartly/MainActivity.kt
+++ app/src/main/java/com/ramniksoftware/chartly/MainActivity.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ramniksoftware.chartly.model.Node
@@ -62,18 +66,37 @@ fun ChartlyScreen(
 ) {
     val state by viewModel.uiState.collectAsState()
 
-    LazyColumn(modifier = modifier.fillMaxSize()) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
         items(state.items) { item ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = (item.depth * 24).dp, top = 8.dp, bottom = 8.dp)
-            ) {
-                Text("•", modifier = Modifier.padding(end = 8.dp))
-                Text(text = item.node.content)
-            }
+            ChartlyRow(flattenedNode = item)
         }
     }
+}
+
+@Composable
+fun ChartlyRow(flattenedNode: FlattenedNode) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = ((flattenedNode.depth * 24) + 8).dp, top = 4.dp, bottom = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Canvas(
+            modifier = Modifier
+                .padding(end = 12.dp)
+                .size(10.dp)
+        ) {
+            drawCircle(
+                color = Color.Gray,
+                radius = size.minDimension / 2 // Perfectly centered circle
+            )
+        }
+        Text(text = flattenedNode.node.content)
+    }
+
 }
 
 @Preview(showBackground = true)
